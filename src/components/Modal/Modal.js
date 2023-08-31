997a9d40101317d10a6c5d707a1ff030776fb206
Mario031281 <taracatus@gmail.com>
2023-08-31 18:18:04 +0300

    class to funk

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Modal from 'react-modal';
 // import { ModalStyled } from './Modal.styled';
 export const customStyles = {
@@ -18,24 +18,41 @@ export const customStyles = {
   },
 };
 Modal.setAppElement('#root');
-export class ImageModal extends Component {
-  state = { isOpen: false };
-  render() {
-    const { isOpen, onRequestClose, image } = this.props;
+export const ImageModal = ({ isOpen, onRequestClose, image }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+      contentLabel="Image Modal"
+    >
+      <img src={image.largeImageURL} alt={image.tags} />
+      <div
+        style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}
+        onClick={onRequestClose}
+      />
+    </Modal>
+  );
+};
 
-    return (
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-        style={customStyles}
-        contentLabel="Image Modal"
-      >
-        <img src={image.largeImageURL} alt={image.tags} />
-        <div
-          style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}
-          onClick={onRequestClose}
-        />
-      </Modal>
-    );
-  }
-}
+// export class ImageModal extends Component {
+//   state = { isOpen: false };
+//   render() {
+//     const { isOpen, onRequestClose, image } = this.props;
+
+//     return (
+//       <Modal
+//         isOpen={isOpen}
+//         onRequestClose={onRequestClose}
+//         style={customStyles}
+//         contentLabel="Image Modal"
+//       >
+//         <img src={image.largeImageURL} alt={image.tags} />
+//         <div
+//           style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}
+//           onClick={onRequestClose}
+//         />
+//       </Modal>
+//     );
+//   }
+// }
